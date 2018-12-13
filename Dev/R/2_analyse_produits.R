7b35ebdb488b0257f5bc2a35b552b20c527a0f42
mcherifa <mcfleurycm@gmail.com>
2018-12-13 17:13:05 +0100

    maj

--- Dev/R/2_analyse_produits.R
+++ Dev/R/2_analyse_produits.R
@@ -18,7 +18,7 @@ chemin <- "/home/menyssa/Calleis"
 
 data_products <- readRDS(file = file.path(chemin,"BDD/clean/data_products.rds"))
 
-####################  Analyse produits 
+####################  Découpage des descriptions 
 
 # Toutes les descriptions sous forme de corpus
 corpus <- data_products$descriptif %>%
@@ -39,22 +39,68 @@ corpus <- corpus %>%
 
 # Matrice de fréquence de mots de tout le corpus
 TDM <- corpus %>% TermDocumentMatrix()
+inspect(TDM)
+
+# Récupère tous les mots du corpus par documents 
+DF <- tidy(TDM)
+DF <- DF[order(DF$term, DF$document),]
+
 m <- as.matrix(TDM)
 v <- sort(rowSums(m),decreasing=TRUE)
 data_terms <- data.frame(word = names(v),freq=v)
 head(data_terms) 
+length(unique(data_terms$word))
 
 # Réduction du nombre de mots en utilisant que les racines
 stem <- wordStem(data_terms$word, language = "french")
 length(unique(stem)) 
 
-# Création du dictionnaire: 
+####################  Analyse description produits 
 # Pour tous ces mots rechercher les associations cohérentes
-# Etape 1 : récupérer les produits vides pour un item concerné
-# Etape 2 : matrice pour chaque mot le vocabulaire correspondant
-# Etape 3 : attribuer l'effet ou non 
-terms <- c("pollution", "hydratation","age","ride", "liftant",  
-           "matifiant","parfum","teint","protections","tache_brune","rougeur",
+# Etape 1 : champs lexical de l'effet recherché
+# Etape 2 : vérification si la description du mot contient un des mots
+# Etape 3 : attribuer l'effet au produit
+
+# Pollution
+findAssocs(TDM, terms = "pollution", corlimit = 0.2)
+mots <- c("pollution","subit","agressions","stress")
+docs <- data.frame(DF[which(DF$term %in% mots),"document"])
+data_products$protection_pollution[as.numeric(docs$document)] <- "Oui"
+
+# Hydratation 
+findAssocs(TDM, terms = "hydratation", corlimit = 0.2)
+mots <- c(unique(grep(pattern = "hydra",DF$term,value = T)),
+          "eau", "desalterer","aqua","aquaserum")
+docs <- data.frame(DF[which(DF$term %in% mots),"document"])
+data_products$hydratant[as.numeric(docs$document)] <- "Oui"
+
+# Age
+findAssocs(TDM, terms = "age", corlimit = 0.2)
+mots <- c("age","agees","ageing","antiage","antiages","antiger",
+          "signes","agefocus","ride","slow","evolution","jeune","premiers")
+docs <- data.frame(DF[which(DF$term %in% mots),"document"])
+data_products$anti_age[as.numeric(docs$document)] <- "Oui"
+
+# Ride
+findAssocs(TDM, terms = "antiride", corlimit = 0.2)
+unique(grep(pattern = "ride",DF$term,value = T))
+mots <- c("ride","rides","antiride","antirides","antiridesnaturellement",
+          "antiridesserum", "deride", "derider","hyalurides","ridee",
+          "ridestachesfermete", "reduites","extraferme", "reduite","combleurs",
+          "aging","extrafermete","ameliorees", "marquees",
+          "ridules","fermete","lisses","lisse","hyaluronique","antiage","botulique")
+docs <- data.frame(DF[which(DF$term %in% mots),"document"])
+data_products$anti_ride[as.numeric(docs$document)] <- "Oui"
+
+# Liftant 
+findAssocs(TDM, terms = "liftant", corlimit = 0.2)
+mots <- c("lifting","liftant","lisse","lissee","progeriliftr",
+          "actilift",unique(grep(pattern = "lift",DF$term,value = T)))
+docs <- data.frame(DF[which(DF$term %in% mots),"document"])
+data_products$liftant[as.numeric(docs$document)] <- "Oui"
+
+terms <- c("protection_pollution", "hydratant","anti_age","anti_ride", "liftant",  
+           "matifiant","no_parfum","teint","protections_uv","tache_brune","rougeur",
            "matin","soir","acne", "lotion", "masque_gommage", "serum",
            "cicatrice", "lavant","sublimateur","correcteur", "anticerne",
            "anti_poche","yeux_levres", "lotion_oculaire","mecanique", 
@@ -65,9 +111,24 @@ terms <- c("pollution", "hydratation","age","ride", "liftant",
            "tous_type_maquillage","apaisant","rafraichissant", 
            "sans_alcool","protection_solaire")
 
-associations <- findAssocs(TDM, terms = terms, corlimit = 0.2)
-freq.terms <- findFreqTerms(TDM, lowfreq = 15)
-plot(TDM, term = freq.terms, corThreshold = 0.12, weighting = T)
+# Matifiant 
+findAssocs(TDM, terms = "matifiant", corlimit = 0.2)
+mots <- unique(grep(pattern = "matif",DF$term,value = T))
+docs <- data.frame(DF[which(DF$term %in% mots),"document"])
+data_products$matifiant[as.numeric(docs$document)] <- "Oui"
+
+# Parfum
+findAssocs(TDM, terms = "teint", corlimit = 0.2)
+mots <- c("teint","unifie","eclatant","lumineux","",
+          "parfumeur","delicat","fleuri","subtil")
+docs <- data.frame(DF[which(DF$term %in% mots),"document"])
+data_products$no_parfum[as.numeric(docs$document)] <- "Non"
+
+# Acne
+findAssocs(TDM, terms = "acne", corlimit = 0.2)
+mots <- c("acne", "boutons", "bouton","comedons","cicatrisante","cicatrices")
+docs <- data.frame(DF[which(DF$term %in% mots),"document"])
+data_products$acne[as.numeric(docs$document)] <- "Oui"
 
 ####################  Graphiques
 
@@ -91,6 +152,16 @@ wordcloud(words = data_terms$word,
           rot.per=0.5, 
           colors=brewer.pal(8, "Dark2"))
 
+# Plot les relations entre les mots les plus fréqents
+freq.terms <- findFreqTerms(TDM, lowfreq = 300)
+vtxcnt <- rowSums(cor(as.matrix(t(TDM[freq.terms,])))>.1)-1
+mycols<-c("#f7fbfe","#f7fbff","#deebf7","#c6dbef","#9ecae1","#6baed6","#4292c6","#2171b5", "#084594")
+vc <- mycols[vtxcnt+1]
+names(vc) <- names(vtxcnt)
+plot(TDM, term = freq.terms,
+     corThreshold = 0.1, 
+     weighting = F,
+     nodeAttrs=list(fillcolor=vc))
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Dev
-Scripts d'analyse des bases de données
+# Calleis
+Author : Ményssa CHERIFA
+[link](https://www.linkedin.com/in/menyssacherifa/) 
+
+## Développement
+Ce repositorie contient les scripts *R* et *Python* nécessaires à l'analyse des bases de données et au développement de modèles dynamiques.
+
+
+
